4da586ff1c07761f06a0e2931cea8a2736b8f1c1
Hisui100P <35003154+Hisui100P@users.noreply.github.com>
2018-01-05 22:06:29 +0900

    d
    d

---
+++ windowsResize.js
@@ -0,0 +1,42 @@
+var container;
+var canvas;
+var timer = 0;
+
+//初期化
+function init() {
+canvas = document.getElementById('canvas');
+container = document.createElement('div');
+container.style.width = window.innerWidth + 'px';
+container.style.height = window.innerHeight + 'px';
+container.style.overflow = 'hidden';
+container.appendChild(canvas);
+document.body.appendChild(container);
+document.body.style.margin = '0px'
+}
+
+//サイズ変更処理
+function resize() {
+container.style.width = window.innerWidth + 'px';
+container.style.height = window.innerHeight + 'px';
+canvas.width = window.innerWidth * window.devicePixelRatio;
+canvas.height = window.innerHeight * window.devicePixelRatio;
+}
+
+window.onload = function () {
+init();
+resize();
+};
+
+//ブラウザの大きさが変わった時に行う処理
+(function () {
+var timer = 0;
+window.onresize = function () {
+if (timer > 0) {
+clearTimeout(timer);
+}
+
+    timer = setTimeout(function () {
+        resize();
+    }, 200);
+};
+}());
